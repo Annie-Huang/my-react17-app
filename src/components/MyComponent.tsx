78d631a83071859c3c9a018e4cacbe2de32ea3b1
AnnieHuang <annie.huang@coles.com.au>
2021-09-02 14:15:10 +1000

    Test we can get the option once select

--- src/components/MyComponent.tsx
+++ src/components/MyComponent.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Select from 'react-select';
 import styled from 'styled-components';
 
-const options = [
+export interface OptionType {
+  value: string;
+  label: string;
+}
+
+const options: OptionType[] = [
   { value: 'chocolate', label: 'Chocolate' },
   { value: 'strawberry', label: 'Strawberry' },
   { value: 'vanilla', label: 'Vanilla' },
 ];
 
 const MyComponent = () => {
+  const [selectedOption, setSelectedOption] = useState<OptionType | null>(null);
+
   return (
     <MyComponentWrapper>
-      <Select options={options} />
+      selectedOption: {JSON.stringify(selectedOption)}
+      <Select onChange={setSelectedOption} options={options} />
     </MyComponentWrapper>
   );
 };
 
 const MyComponentWrapper = styled.div`
   max-width: 400px;
-  margin: 100px auto;
+  margin: 100px 0 0 50px;
 `;
 
 export default MyComponent;
